b9e40da30b64d14c0687885bc296201e35e0bb97
YeeShin504 <58208381+YeeShin504@users.noreply.github.com>
2026-03-25 00:37:16 +0800

    Fix add command description

--- src/main/java/cms/logic/commands/AddCommand.java
+++ src/main/java/cms/logic/commands/AddCommand.java
@@ -39,7 +39,7 @@ public class AddCommand extends Command {
             + "[" + PREFIX_TAG + "TAG]...\n"
             + "Example: " + COMMAND_WORD + " "
             + PREFIX_NAME + "John Doe "
-            + PREFIX_NUSID + "A1234567 "
+            + PREFIX_NUSID + "A1234567E "
             + PREFIX_ROLE + "student "
             + PREFIX_SOCUSERNAME + "johndoe "
             + PREFIX_GITHUBUSERNAME + "johndoe "
